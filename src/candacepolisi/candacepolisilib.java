9e1e2f16ac628fbc2459ae81b912206f2b3e875a
BT_1N3_18 <cpolisi6526@bths.edu>
2018-10-05 08:21:58 -0400

    7

--- src/candacepolisi/candacepolisilib.java
+++ src/candacepolisi/candacepolisilib.java
@@ -2,10 +2,10 @@ package candacepolisi;
 
 public class candacepolisilib {
 
-    public static int sumUp(int x){
-        int r= 0;
-        for (int c=x; c>0; c--){
-            r+=c;
+    public static int sumUp(int x) {
+        int r = 0;
+        for (int c = x; c > 0; c--) {
+            r += c;
         }
         return r;
     }
@@ -25,65 +25,62 @@ public class candacepolisilib {
         for (int c = pal.length(); c > 0; c--) {
             check = c + pal.substring(c, c + 1);
         }
-        if (check.equals(pal)){
+        if (check.equals(pal)) {
             return "true";
-        }
-        else {
+        } else {
             return "false";
         }
     }
 
-    public static boolean fibanacci(int z){
-        int a= 1;
-        int b= 1;
-        int c= 0;
-        while (z>a && z>b && z>c){
-            c= a +b;
-            b= c+ a;
-            a= c+b;
+    public static boolean fibanacci(int z) {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        while (z > a && z > b && z > c) {
+            c = a + b;
+            b = c + a;
+            a = c + b;
         }
-        return (z==a | z==b | z==c);
+        return (z == a | z == b | z == c);
     }
 
-    public static String quad(double a, double b, double c){
+    public static String quad(double a, double b, double c) {
         // -b +- sq root b^2 -4ac /2a
-        double r=0 ;
+        double r = 0;
         // sqs b
-        double s= b*b;
-        String res= "";
+        double s = b * b;
+        String res = "";
         //gets stuff under the sqrt
-        s= s-4*a*c;
-        if (s<0){
+        s = s - 4 * a * c;
+        if (s < 0) {
             return "imaginary numbers sorry";
-        }
-        else{
-            s= Math.sqrt(s);
-            r=s;
-            s= (-b + s)/ (2*a);
-            r= (-b -s)/ (2*a);
-            res= "" + s + " and " + r;
+        } else {
+            s = Math.sqrt(s);
+            r = s;
+            s = (-b + s) / (2 * a);
+            r = (-b - s) / (2 * a);
+            res = "" + s + " and " + r;
             return res;
         }
     }
 
-    public static String cutOut(String str, String sh){
-        String r= "";
-        int c= str.indexOf(sh);
-        r= str.substring(0,c) + str.substring(c+sh.length()-1);
+    public static String cutOut(String str, String sh) {
+        String r = "";
+        int c = str.indexOf(sh);
+        r = str.substring(0, c) + str.substring(c + sh.length() - 1);
         return r + " " + sh;
     }
 
-    public static int LCM(int a, int b){
-        int m=a;
-        if ((a==1)||(b==1)){
-            m=1;
+    public static int LCM(int a, int b) {
+        int m = a;
+        if ((a == 1) || (b == 1)) {
+            m = 1;
         }
-        if ((a==0)||(b==0)) {
+        if ((a == 0) || (b == 0)) {
             m = 0;
-        }
-        else{
-            while (m%b>0){
-                m=m+a;
+        } else {
+            while (m % b > 0) {
+                m = m + a;
             }
         }
         return m;
@@ -93,8 +90,8 @@ public class candacepolisilib {
         int r = 0;
         String le = "";
         int i = 1;
-        for (int d =0; a.length()>d; d++) {
-            le = a.substring(d,d+1);
+        for (int d = 0; a.length() > d; d++) {
+            le = a.substring(d, d + 1);
             i = b.indexOf(le);
             if (i >= 0) {
                 r++;
@@ -102,8 +99,7 @@ public class candacepolisilib {
                     b = b.substring(0, i) + b.substring(i + 1);
                     i = b.indexOf(le);
                 }
-            }
-            else {
+            } else {
                 i = c.indexOf(le);
                 if (i >= 0) {
                     r++;
@@ -114,8 +110,9 @@ public class candacepolisilib {
                 }
             }
         }
-        for (int d =0; b.length()>d; d++) {
-            le = b.substring(d, d+1);
+        for (int d = 0; b.length() - 2 > d; d++) {
+//// changed to minus 2 idk why but it works
+            le = b.substring(d, d + 1);
             i = b.indexOf(le);
             if (i >= 0) {
                 r++;
@@ -127,33 +124,32 @@ public class candacepolisilib {
         }
         return r;
     }
-    public static String vig(String og, String sh){
-        String r="";
-        String alphabet= "abcdefghijklmnopqrstuvwxyz";
-        int ogl= 0;
-        int shl= 0;
-        String let= "";
-        String shift= "";
-        int d= 0;
-        int rnum= 0;
+
+    public static String vig(String og, String sh) {
+        String r = "";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int ogl = 0;
+        int shl = 0;
+        String let = "";
+        String shift = "";
+        int d = 0;
+        int rnum = 0;
         int c;
-        for (c = 0; c < (og.length() - 1); c++){
-            let= og.substring(c,c+1);
-            ogl= alphabet.indexOf(let);
-            if (d> sh.length()-1){
-                d=0;
+        for (c = 0; c < (og.length()); c++) {
+            let = og.substring(c, c + 1);
+            ogl = alphabet.indexOf(let);
+            if (d > sh.length() - 1) {
+                d = 0;
             }
-            shift= sh.substring(d,d+1);
-            shl= alphabet.indexOf(shift);
+            shift = sh.substring(d, d + 1);
+            shl = alphabet.indexOf(shift);
             d++;
-            rnum= ogl + shl;
-            if (rnum> alphabet.length()-1){
-                rnum= alphabet.length()-1- rnum;
+            rnum = ogl + shl;
+            if (rnum > alphabet.length() - 1) {
+                rnum = rnum - alphabet.length() - 1;
             }
-            r= r + alphabet.substring(rnum, rnum+1);
+            r = r + alphabet.substring(rnum, rnum + 1);
         }
         return r;
     }
-
-
 }
